e1626172c63beabf0bf6a2c782a13b0ee1e02940
joe mcgee <jrmcgee@asu.edu>
2022-03-01 18:25:54 +0900

    added additional code

--- src/Searchmovies.js
+++ src/Searchmovies.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 
 export default function Searchmovies(){
+    const [query, setQuery] = useState('');
+    const [movies, setMovies] = useState([]);
+    const Searchmovies = async(e) =>{
+        e.preventDefault(); //prevents the link from appearing
+       
+       
+        const url= `https://api.themoviedb.org/3/search/movie?api_key=cef4c4b7f7940103893a439035dd85df&language=en-US&query=${query}&page=1&include_adult=false`;
+        try{
+        const res = await fetch(url);
+        const data  = await res.json();
+        console.log(data.results);
+        setMovies(data.results);
+        }catch(err){
+            console.error(err);
+        }
+}
     return(
             <div>
-                <form className="Form">
+                <form className="Form" onSubmit={Searchmovies}>
                     <label className="label"
-                    htmlFor="query"> Movies 
+                    htmlFor="query"> Find a movie
                     </label>
-                    <input className="input" type="text" name="query" placeholder="Search"></input>
+                    <input className="input" type="text" name="query" placeholder="Search" value = {query} onChange={(e) =>setQuery(e.target.value)}></input>
                     <button className = "button-submit" type = "submit">Submit</button>
                 </form>
-                
             </div>
     )
 }
